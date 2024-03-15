36d2972690b284a17572f34791177c232fa274f8
pramodpilane <pramodpilane8p@gmail.com>
2024-03-15 12:22:19 +0530

    shifted contents of module3 to module2

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import Header from './Header&Footer/Header';
 import Upload_file from './Module1/upload_file';
 import About from './Module1/about';
 import Module2 from './Module2/Module2';
-import Module3 from './Module3/Module3';
 import Footer from './Header&Footer/Footer';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
 function App() {
   const [color, setColor] = React.useState('primary');
+  const [selectedFiles, setSelectedFiles] = React.useState([]);
 
   return (
   <>
@@ -17,10 +17,9 @@ function App() {
       <div>
         <Header color={color} setColor={setColor} />
         <Routes>
-          <Route exact path="/" element={<Upload_file />} />
+          <Route exact path="/" element={<Upload_file selectedFiles={selectedFiles} setSelectedFiles={setSelectedFiles}/>} />
           <Route exact path="/about" element={<About />} />
           <Route exact path="/prompt" element={<Module2 />} />
-          <Route exact path="/solution/:question" element={<Module3 />} />
         </Routes>
         <Footer color={color} />
       </div>
