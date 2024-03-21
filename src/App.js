81f072a7780a467152c3ac7b75ecaf7b79646265
pramodpilane <pramodpilane8p@gmail.com>
2024-03-21 19:14:13 +0530

    done with all suggestions

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Header from './Header&Footer/Header';
 import Upload_file from './Module1/upload_file';
 import About from './Module1/about';
 import Module2 from './Module2/Module2';
-import Quiz from './Module2/QuizContent';
+import Quiz from './Module2/Quiz';
 import QuizResult from './Module2/QuizResult';
 import Faq from './FAQ/Faq';
 import Footer from './Header&Footer/Footer';
@@ -14,8 +14,8 @@ function App() {
   const [color, setColor] = React.useState('primary');
   const [selectedFiles, setSelectedFiles] = React.useState([]);
   const [faq, setFaq] = React.useState();
+  const [quiz,setQuiz] = React.useState();
   const [suggestion, setSuggestion] = React.useState();
-
   return (
   <>
     <Router>
@@ -23,12 +23,12 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Upload_file selectedFiles={selectedFiles} setSelectedFiles={setSelectedFiles} setSuggestion={setSuggestion}/>} />
           <Route exact path="/about" element={<About />} />
-          <Route exact path="/prompt" element={<Module2 setFaq={setFaq} suggestion={suggestion}/>} />
+          <Route exact path="/prompt" element={<Module2 setQuiz={setQuiz} setFaq={setFaq} suggestion={suggestion}/>} />
           <Route exact path="/prompt/faq" element={<Faq faq={faq}/>} />
-          <Route exact path="/prompt/quiz" element={<Quiz />} />
+          <Route exact path="/prompt/quiz" element={<Quiz quiz={quiz}/>} />
           <Route exact path="/prompt/quiz/quizResult" element={<QuizResult />} />
          </Routes>
-        {/* <Footer color={color} /> */}
+        <Footer color={color} />
     </Router>
   </>
   );
